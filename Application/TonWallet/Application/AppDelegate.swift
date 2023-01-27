09d5a69a2f6ca4af879d3b253728449536012591
Maksim Khrabryi <Max.xaker41@mail.ru>
2023-01-28 02:10:03 +0400

    ton-kotlin lib

--- Application/TonWallet/Application/AppDelegate.swift
+++ Application/TonWallet/Application/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import shared
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,6 +7,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let ton = Ton()
+        let liteClient = ton.doInitLiteClient()
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = RootNavigationController(rootViewController: MainViewController())
