1d346c9a8e6d3bf2a7630ba75f22a5ed331dc741
Maksim Khrabryi <Max.xaker41@mail.ru>
2023-03-18 17:04:35 +0400

    show wallet vc when user have wallets

--- Application/TonWallet/Application/AppDelegate.swift
+++ Application/TonWallet/Application/AppDelegate.swift
@@ -7,10 +7,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        
+        let rootVC: UIViewController
+         
+        if WalletManager.shared.wallets.isEmpty {
+            rootVC = CreateViewController()
+        } else {
+            rootVC = TabBarViewController()
+        }
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = RootNavigationController(rootViewController: TabBarViewController())
+        window?.rootViewController = RootNavigationController(rootViewController: rootVC)
         window?.makeKeyAndVisible()
         
         return true
